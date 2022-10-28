feb9c9481f645bb3b06edbc94e4bc1f3c2085a4e
Kingsley Victor <javaprodigy56@gmail.com>
2022-10-28 06:56:54 +0100

    deploy to 4 chains

--- scripts/deploy_actions.js
+++ scripts/deploy_actions.js
@@ -1,12 +1,25 @@
 const { ethers, network } = require("hardhat");
 const path = require("path");
 const fs = require("fs");
+const axios = require("axios");
+
+const coinGeckID = {
+  56: "binancecoin",
+  32520: "bitrise-token",
+  311: "omax-token",
+  86: "gatechain-token"
+};
 
 (async () => {
   console.log("---------- Deploying to chain %d ----------", network.config.chainId);
   const StakingPoolActionsFactory = await ethers.getContractFactory("StakingPoolActions");
+  const cgID = coinGeckID[network.config.chainId];
+  const { data } = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${cgID}&vs_currencies=usd`);
+  const valInUSD = data[cgID].usd;
+  console.log("-----", valInUSD);
+  const valEther = 120 / valInUSD;
 
-  let stakingPoolActions = await StakingPoolActionsFactory.deploy(ethers.utils.parseEther("0.0003"));
+  let stakingPoolActions = await StakingPoolActionsFactory.deploy(ethers.utils.parseEther(valEther.toString()));
   stakingPoolActions = await stakingPoolActions.deployed();
 
   const location = path.join(__dirname, "../staking_pool_actions_addresses.json");
